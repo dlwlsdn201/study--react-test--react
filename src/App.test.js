605746bbe566a8c4f46eaa976dbf5ada2267b50b
leejinw <leejinw@nxtcorp.co.kr>
2023-01-02 09:58:16 +0900

    :construction: counter 앱에 대한 테스트 코드 작성

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
+import { toHaveTextContent } from '@testing-library/jest-dom/dist/matchers';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('The counter starts at 0', () => {
+	render(<App />); // App 컴포넌트 렌더링 (root 컴포넌트 렌더링)
+
+	// screen object를 이용해서 원하는 element에 접근 (id로 접근)
+	const counterElement = screen.getByTestId('counter');
+
+	// id가 counter인 element의 텍스트가 0인지 테스트
+	expect(counterElement).toHaveTextContent(0);
 });
