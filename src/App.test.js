516282a60d7cb9cbf6bf77d13ac19653b08aa1e7
dlwlsdn201 <leejinw@nxtcorp.co.kr>
2023-01-03 10:08:04 +0900

    :memo: counter 테스트 코드 주석 상태 해제

--- src/App.test.js
+++ src/App.test.js
@@ -5,10 +5,10 @@ test('The counter starts at 0', () => {
 	render(<App />); // App 컴포넌트 렌더링 (root 컴포넌트 렌더링)
 
 	// screen object를 이용해서 원하는 element에 접근 (id로 접근)
-	// const counterElement = screen.getByTestId('counter');
+	const counterElement = screen.getByTestId('counter');
 
 	// id가 counter인 element의 텍스트가 0인지 테스트
-	// expect(counterElement).toHaveTextContent(0);
+	expect(counterElement).toHaveTextContent(0);
 });
 
 test('minus button has correct text', () => {
