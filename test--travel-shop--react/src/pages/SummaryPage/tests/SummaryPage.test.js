92e236490f8217a71312fe0fea87873a5756b525
dlwlsdn201 <leejinw@nxtcorp.co.kr>
2023-01-13 09:49:16 +0900

    :hammer: resolved #1 앱 구조 설계에 따른 기본 코드 파일 생성

---
+++ test--travel-shop--react/src/pages/SummaryPage/tests/SummaryPage.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import SummaryPage from '../SummaryPage';
+
+it('checkbox and button', () => {
+	render(<SummaryPage />);
+	const checkbox = screen.getByRole('checkbox', {
+		name: '주문하려는 것을 확인하셨나요?'
+	});
+
+	expect(checkbox.checked).toEqual(false); // 최초 렌더링 시, 체크박스는 false 이어야함.
+
+	const confirmButton = screen.getByRole('button', {
+		name: '주문 확인'
+	});
+	expect(confirmButton.disabled).toBeTruthy();
+});
