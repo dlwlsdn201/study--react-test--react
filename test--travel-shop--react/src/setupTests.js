80fd7facfbfdbaed216c5666e74f877df6ae2176
dlwlsdn201 <leejinw@nxtcorp.co.kr>
2023-01-15 16:29:41 +0900

    :white_check_mark: msw 서버 통신 테스트 코드 작성

--- test--travel-shop--react/src/setupTests.js
+++ test--travel-shop--react/src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { server } from './mocks/server';
+
+// 모든 테스트 시작 전, server을 listen 상태로 만듬
+beforeAll(() => server.listen());
+
+// 각 테스트들이 끝날 때마다, server의 handler 들을 리셋 (다른 테스트들에 영향이 가지 않게 하기 위함)
+afterEach(() => server.resetHandlers());
+
+// 모든 테스트 끝난 후, server 종료
+afterAll(() => server.close());
