8d086d0d843dc641557d62207566a97c8b9c2ff6
dlwlsdn201 <leejinw@nxtcorp.co.kr>
2023-02-04 14:39:33 +0900

    :construction: 상품, 옵션 선택에 따른 총 가격 값 계산 로직 구현

--- test--travel-shop--react/src/contexts/OrderContext.js
+++ test--travel-shop--react/src/contexts/OrderContext.js
@@ -1,17 +1,64 @@
-const { createContext, useMemo, useState } = require('react');
+const { createContext, useMemo, useState, useEffect } = require('react');
 
 export const OrderContext = createContext();
 
+const pricePerItem = {
+	products: 1000,
+	options: 500
+};
+
+function calculateSubtotal(orderType, orderCounts) {
+	let optionCounts = 0;
+	for (const count of orderCounts[orderType].values()) {
+		optionCounts += count;
+	}
+
+	return optionCounts + pricePerItem[orderType];
+}
 
 export const OrderContextProvider = (props) => {
-  const [orderCounts, setOrderCounts] = useState({
-    products: new Map(),
-    options: new Map(),
-  })
+	const [orderCounts, setOrderCounts] = useState({
+		products: new Map(),
+		options: new Map()
+	});
 
-  const value = useMemo(() => {
-    return [{...orderCounts}]
-  }, [orderCounts]);
-  
-  return <OrderContext.Provider value={...props}/>
-}
+	const [totals, setTotals] = useState({
+		products: 0,
+		options: 0,
+		total: 0
+	});
+
+	useEffect(() => {
+		const productsTotal = calculateSubtotal('products', orderCounts);
+		const optionsTotal = calculateSubtotal('options', orderCounts);
+		const total = productsTotal + optionsTotal;
+
+		setTotals({
+			products: productsTotal,
+			options: optionsTotal,
+			total
+		});
+	}, [orderCounts]);
+
+	const value = useMemo(() => {
+		/**
+		 *
+		 * @param {string} itemName 상품명 (America, England 등)
+		 * @param {number} newItemCount 상품 수량
+		 * @param {'products' | 'options'} orderType 데이터 타입 (상품 데이터, 옵션 데이터)
+		 */
+		function updateItemCount(itemName, newItemCount, orderType) {
+			const newOrderCounts = { ...orderCounts };
+
+			//
+			const orderCountsMap = orderCounts[orderType]; // 기존 state
+			orderCountsMap.set(itemName, parseInt(newItemCount)); // Map 내부에 업데이트할 key(itemName)의 value를 Number타입의 newItemCount 로 업데이트
+
+			setOrderCounts(newOrderCounts);
+		}
+
+		return [{ ...orderCounts, totals }, updateItemCount];
+	}, [orderCounts, totals]);
+
+	return <OrderContext.Provider value={value} {...props} />;
+};
