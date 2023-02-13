6ebf5deaac60a0f4fd4c964990f77f1ca4d160eb
dlwlsdn201 <leejinw@nxtcorp.co.kr>
2023-02-13 09:55:49 +0900

    :hammer: 주문 완료 POST API 코드 작성 (service worker 핸들러 함수 내부에 작성)

--- test--travel-shop--react/src/mocks/handlers.ts
+++ test--travel-shop--react/src/mocks/handlers.ts
@@ -26,5 +26,12 @@ export const handlers = [
 				}
 			])
 		);
+	}),
+
+	// 주문 완료 POST 요청 API
+	rest.post('http://localhost:5000/order', (req, res, ctx) => {
+		const dummyData = [{ orderNumber: 231234214515, price: 2000 }];
+
+		return res(ctx.json(dummyData));
 	})
 ];
