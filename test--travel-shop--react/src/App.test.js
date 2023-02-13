c3e7d85c74a405947a99305ac7c8960887c50477
dlwlsdn201 <leejinw@nxtcorp.co.kr>
2023-02-13 09:54:34 +0900

    :white_check_mark: 주문 완료 페이지 테스트 코드 작성

--- test--travel-shop--react/src/App.test.js
+++ test--travel-shop--react/src/App.test.js
@@ -67,4 +67,26 @@ test('From order to order completion', async () => {
 		name: '주문 확인'
 	});
 	userEvent.click(confirmOrderButton); // 버튼 클릭
+
+	////////////////// [주문 완료 페이지] //////////////////
+
+	// 1. 주문 완료 내역 데이터를 불러오기 전 Loading 텍스트를 가지고 있는 Element 마운트 여부
+	const loading = screen.getByText(/loading/i);
+	expect(loading).toBeInTheDocument();
+
+	// 2. 주문 성공 화면의 Header Element 마운트 여부
+	const completeHeader = await screen.findByRole('heading', {
+		name: '주문이 성공했습니다.'
+	});
+	expect(completeHeader).toBeInTheDocument();
+
+	// 3. 주문 완료 내역 데이터 불러오기가 완료되었을 경우 Loading 텍스트를 가지고 있는 Element 언마운트 여부
+	const loadingDisappeared = screen.queryByText('loading');
+	expect(loadingDisappeared).not.toBeInTheDocument();
+
+	// 4. [첫 페이지로] 버튼 기능 동작 여부
+	const firstPageButton = screen.getByRole('button', {
+		name: '첫페이지로'
+	});
+	userEvent.click(firstPageButton);
 });
